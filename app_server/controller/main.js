a908e58300d40a396d771310da865bad0b46b7eb
shivareddy981 <shivak99600@gmail.com>
2024-08-01 15:49:22 +0530

    first commit

---
+++ app_server/controller/main.js
@@ -0,0 +1,7 @@
+/* GET home page*/
+module.exports.index=function(req,res){
+    res.render('index',{title:'Express'});
+};
+
+
+
--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var ctrlMain = require('../controller/main');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
